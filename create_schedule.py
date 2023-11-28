d2eb6d23f4c00a55d9c2c5f6e16ae432a91eac74
Peng Liu <liupengufl@gmail.com>
2023-11-28 16:55:12 -0500

    first time check

--- create_schedule.py
+++ create_schedule.py
@@ -1,49 +1,100 @@
-from ics import Calendar, Event
+from icalendar import Calendar, Event, Alarm
 from datetime import datetime, timedelta
 import pytz
 
-def create_event(day, start_time, end_time, description, tz):
-    """Creates an event for the calendar."""
+def create_event(day, start_time, end_time, description, tz, reminder_minutes=15):
+    """Creates an event for the calendar and adds a reminder."""
     event = Event()
-    event.name = description
-    start_datetime = datetime.strptime(f'{day} {start_time}', '%Y-%m-%d %H:%M')
-    end_datetime = datetime.strptime(f'{day} {end_time}', '%Y-%m-%d %H:%M')
-    # Localize the datetime objects with the specified timezone
-    start_datetime = tz.localize(start_datetime)
-    end_datetime = tz.localize(end_datetime)
-    event.begin = start_datetime
-    event.end = end_datetime
+    event.add('summary', description)
+
+    # Parse the datetime and adjust for timezone
+    start_datetime = tz.localize(datetime.strptime(f'{day} {start_time}', '%Y-%m-%d %H:%M'))
+    end_datetime = tz.localize(datetime.strptime(f'{day} {end_time}', '%Y-%m-%d %H:%M'))
+    event.add('dtstart', start_datetime)
+    event.add('dtend', end_datetime)
+
+    # Add an alarm to the event
+    alarm = Alarm()
+    alarm.add("action", "DISPLAY")
+    alarm.add('description', f'Reminder for {description}')
+    alarm.add("trigger", timedelta(minutes=-reminder_minutes))
+    event.add_component(alarm)
+
     return event
 
 # Create a new calendar
 cal = Calendar()
 
-# Define your time zone (change 'America/New_York' to your time zone)
+# Define your time zone
 time_zone = pytz.timezone('America/New_York')
 
 # Define your work week (Monday to Saturday)
-work_days = [datetime(2023, 11, 27) + timedelta(days=i) for i in range(6)]  # Example week starting from 2023-11-27
+start_date = datetime(2023, 11, 28)
+work_days = [(start_date + timedelta(days=i)).strftime('%Y-%m-%d') for i in range(6)]
 
-# Define the time slots for your tasks
-time_slots = [
-    ('08:00', '10:00', 'High-priority tasks (VCA Model or Proposal Writing)'),
-    ('10:00', '12:00', 'Continue high-priority work or Teaching Preparation'),
-    ('13:00', '15:00', 'Work on GatorBrain or HeadNeck projects'),
-    ('15:00', '17:00', 'Continue with ongoing tasks or Interpretable AI project'),
-    ('18:00', '20:00', 'Resume work with less intense concentration'),
-    ('20:00', '22:00', 'Wrap up and prepare for the next day')
-]
 
-# Add events to the calendar for each work day
-for day in work_days:
-    day_str = day.strftime('%Y-%m-%d')
-    for start, end, desc in time_slots:
-        event = create_event(day_str, start, end, desc, time_zone)
-        cal.events.add(event)
+# Define the detailed tasks for each time slot
+detailed_tasks = {
+    '08:00-10:00': {
+        'Monday': 'VCA Model Development ',
+        'Tuesday': 'Proposal Writing - Modulating Striatal-Amygdala Pathway',
+        'Wednesday': 'VCA Model Development ',
+        'Thursday': 'Proposal Writing - Modulating Striatal-Amygdala Pathway',
+        'Friday': 'Teaching Preparation  - Content Review and Interactive Elements',
+        'Saturday': 'GatorBrain Project '
+    },
+    '10:00-12:00': {
+        'Monday': 'GatorBrain Project - Research and Collaboration Meetings',
+        'Tuesday': 'HeadNeck Outcome ',
+        'Wednesday': 'GatorBrain Project ',
+        'Thursday': 'HeadNeck Outcome - Data Analysis and Report Drafting',
+        'Friday': 'Interpretable AI Research - Literature Review and Testing',
+        'Saturday': 'HeadNeck CBCT - Development'
+    },
+    '13:00-15:00': {
+        'Monday': 'Teaching Preparation - Content Review and Interactive Elements',
+        'Tuesday': 'VCA Model Development',
+        'Wednesday': 'Teaching Preparation - Content Review and Interactive Elements',
+        'Thursday': 'VCA Model Development',
+        'Friday': 'HeadNeck CBCT - Development',
+        'Saturday': 'Writing Proposal - Modulating Striatal-Amygdala Pathway'
+    },
+    '15:00-17:00': {
+        'Monday': 'Proposal Writing - Modulating Striatal-Amygdala Pathway',
+        'Tuesday': 'Interpretable AI - Algorithm Development',
+        'Wednesday': 'Proposal Writing - Modulating Striatal-Amygdala Pathway',
+        'Thursday': 'Interpretable AI - Algorithm Development',
+        'Friday': 'HeadNeck Outcome',
+        'Saturday': 'Teaching Preparation - Finalizing & Recording'
+    },
+    '18:00-20:00': {
+        'Monday': 'Administrative Tasks and Light Research',
+        'Tuesday': 'Administrative Tasks and Light Research',
+        'Wednesday': 'HeadNeck CBCT - Development',
+        'Thursday': 'Administrative Tasks and Light Research',
+        'Friday': 'Administrative Tasks and Light Research',
+        'Saturday': 'Administrative Tasks and Light Research'
+    },
+    '20:00-22:00': {
+        'Monday': 'Review Day’s Progress and Plan Next Day',
+        'Tuesday': 'Review Day’s Progress and Plan Next Day',
+        'Wednesday': 'Review Day’s Progress and Plan Next Day',
+        'Thursday': 'GatorBrain Project',
+        'Friday': 'Review Day’s Progress and Plan Next Day',
+        'Saturday': 'Review Day’s Progress and Plan Next Day'
+    }
+}
+# Add events to the calendar for each time slot and work day
+for day, day_str in zip(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'], work_days):
+    for time_slot, tasks in detailed_tasks.items():
+        start_time, end_time = time_slot.split('-')
+        description = tasks.get(day, 'General Work')
+        event = create_event(day_str, start_time, end_time, description, time_zone)
+        cal.add_component(event)
 
 # Save the calendar to an .ics file
 file_name = 'Custom_Work_Schedule.ics'
-with open(file_name, 'w') as f:
-    f.writelines(cal)
+with open(file_name, 'wb') as f:
+    f.write(cal.to_ical())
 
 print(f"Calendar file '{file_name}' created successfully.")
